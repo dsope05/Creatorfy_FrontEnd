393aa2df2a54a7627949192d0778eb2a517a8ca3
msineath <mikesineath@gmail.com>
2022-04-05 20:03:47 -0400

    added test page for dynamic loading of banner data

---
+++ pages/services/[id].js
@@ -0,0 +1,12 @@
+import Banner from '../../components/Banner/index';
+import styles from '../../styles/ServicePage.module.css';
+
+const ServicePage = () => {
+    return(
+        <div className={styles.container}>
+            <Banner />
+        </div>
+    )
+}
+
+export default ServicePage;
