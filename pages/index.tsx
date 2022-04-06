c9a341672241f79910c4cc12f231a8b927d795f7
msineath <mikesineath@gmail.com>
2022-04-06 17:41:47 -0400

    added force https for share button on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import CreatorCard from '../components/creatorCard'
 import Services from '../components/services'
 import Banner from '../components/Banner'
 
+/* COMMENT IN THE LINE BELOW WHEN GOING TO PRODUCTION MODE!!! SHARE BUTTON ONLY WORKS WITH HTTPS AND MOBILE DEVICES DEFAULT TO HTTP*/
+
+// if(window.location.protocol !== "https:") window.location.protocol = "https:";
 
 
 export default function Profile() {
