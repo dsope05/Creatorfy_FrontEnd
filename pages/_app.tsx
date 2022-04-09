5832a12d705679e60a93e006d661032fb5ac50ff
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2022-04-08 22:38:54 -0700

    Add Bootstrap with Sass Theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,12 @@ import { ApolloProvider } from '@apollo/client';
 import { ModalProvider } from '../components/providers/ModalProvider';
 import { useApollo } from '../graphql/useApollo';
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
 import Layout from '../components/layout';
+
+// Add bootstrap css & custom theme & global CSS
+import 'bootstrap/dist/css/bootstrap.css';
+import '../styles/bootstrapTheme.scss';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const apolloClient = useApollo(pageProps.initialApolloState);
